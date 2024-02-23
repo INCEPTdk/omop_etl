d3fc36d4137ea53822bbca30f1945cae9945f18f
Nir-edence <nir.assaraf@edence.health>
2024-02-23 09:43:58 +0100

    add count

--- etl/transform/location.py
+++ etl/transform/location.py
@@ -1,6 +1,7 @@
 """Location transformations"""
 import logging
 
+from ..models.omopcdm54.health_systems import Location
 from ..sql.location import LOCATION_INSERT
 from ..util.db import AbstractSession
 
@@ -11,4 +12,7 @@ def transform(session: AbstractSession) -> None:
     """Run the location transformation"""
     logger.info("Starting the location transformation... ")
     session.execute(LOCATION_INSERT)
-    logger.info("Location transformation finished successfully!")
+    logger.info(
+        "Location transformation finished successfully with count %s",
+        session.query(Location).count(),
+    )
