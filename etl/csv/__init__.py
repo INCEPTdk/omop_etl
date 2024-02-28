32885f8fae6a620cf335e29be0f2ad6dc54c4422
Benjamin Skov Kaas-Hansen <epiben@hey.com>
2024-02-28 22:15:42 +0100

    Simplify shak lookup logic

--- etl/csv/__init__.py
+++ etl/csv/__init__.py
@@ -3,6 +3,8 @@ from typing import Final
 
 import pandas as pd
 
-LOOKUP_DF: Final[pd.DataFrame] = pd.read_csv(
-    "etl/csv/shak_lookup.tsv", sep="\t", dtype=str
+rows = pd.read_csv("etl/csv/shak_lookup.tsv", sep="\t", dtype=str).itertuples(
+    index=False, name="CareSite"
 )
+
+SHAK_LOOKUP: Final[dict] = {r.department_shak_code: r._asdict() for r in rows}
