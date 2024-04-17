799b433fa97665911184147882fc6c322d4e2720
Benjamin Skov Kaas-Hansen <epiben@hey.com>
2024-04-17 13:46:09 +0200

    add misc helper functions

---
+++ etl/util/misc.py
@@ -0,0 +1,20 @@
+"""Miscellaneous utility functions"""
+
+from itertools import chain
+from typing import Any
+
+from sqlalchemy import DateTime
+
+
+def flatten_to_set(lst: list = None) -> set:
+    """Flatten a list of iterables and return a set of unique values"""
+
+    return set(x for x in chain(*lst) if x is not None)
+
+
+def find_datetime_columns(model: Any = None) -> list:
+    """Find datetime columns in a model"""
+
+    return [
+        c.name for c in model.__table__.columns if isinstance(c.type, DateTime)
+    ]
