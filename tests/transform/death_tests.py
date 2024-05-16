2d265effce5938761b655f39e5ef4854875a77d2
Benjamin Skov Kaas-Hansen <epiben@hey.com>
2024-05-16 14:17:54 +0200

    start porting death tests to duckdb

--- tests/transform/death_tests.py
+++ tests/transform/death_tests.py
@@ -10,10 +10,10 @@ from etl.models.omopcdm54.clinical import (
 from etl.models.source import Person as SourcePerson
 from etl.transform.death import transform as death_transform
 from etl.util.db import make_db_session, session_context
-from tests.testutils import PostgresBaseTest, base_path, write_to_db
+from tests.testutils import DuckDBBaseTest, base_path, write_to_db, enforce_dtypes, assert_dataframe_equality
 
 
-class DeathTransformationTest(PostgresBaseTest):
+class DeathTransformationTest(DuckDBBaseTest):
     SOURCE_MODELS = [SourcePerson]
     TARGET_MODEL = [OmopPerson, OmopDeath]
     INPUT_OMOP_PERSON = f"{base_path()}/test_data/death/in_omop_person.csv"
@@ -34,19 +34,25 @@ class DeathTransformationTest(PostgresBaseTest):
         self._drop_tables_and_schema(self.SOURCE_MODELS, schema='source')
         self._drop_tables_and_schema(self.TARGET_MODEL, schema='omopcdm')
 
-    def _insert_test_data(self, engine):
-        write_to_db(engine, self.source_person_in, SourcePerson.__tablename__, schema=SourcePerson.metadata.schema)
-        write_to_db(engine, self.omop_person_in, OmopPerson.__tablename__, schema=OmopPerson.metadata.schema)
+    def _insert_test_data(self, session):
+        write_to_db(session, self.source_person_in, SourcePerson.__tablename__, schema=SourcePerson.metadata.schema)
+        write_to_db(session, self.omop_person_in, OmopPerson.__tablename__, schema=OmopPerson.metadata.schema)
 
     def test_transform(self):
-        self._insert_test_data(self.engine)
         with session_context(make_db_session(self.engine)) as session:
+            self._insert_test_data(session)
             death_transform(session)
 
-        # Only queries the columns that exist in the self.test_data_in
-        result = select(self.expected_cols)
-        result_df = pd.read_sql(result, self.engine)
-        self.assertTrue(result_df.compare(self.expected_df).empty)
+            # Only queries the columns that exist in the self.test_data_in
+            result = str(
+                select(self.expected_cols).compile(
+                    compile_kwargs={"literal_binds": True}
+                )
+            )
+            result_df = pd.read_sql(result, con=session.connection().connection)
+
+        result_df = enforce_dtypes(self.expected_df, result_df)
+        assert_dataframe_equality(result_df, self.expected_df, index_col="person_id")
 
 
 __all__ = ["DeathTransformationTest"]
