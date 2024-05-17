59c6c172f8e88256b55c5e540ce51df2023a6cfa
Benjamin Skov Kaas-Hansen <epiben@hey.com>
2024-05-18 00:56:16 +0200

    adapt DB tests, especially w.r.t. floats and decimals

--- tests/util/dbtests.py
+++ tests/util/dbtests.py
@@ -1,4 +1,5 @@
 import json
+import math
 from typing import Any, Final
 
 import pandas as pd
@@ -26,7 +27,7 @@ class DBDuckDBTests(DuckDBBaseTest):
 
     class DummyTable(TestModelBase, PKIdMixin):
         __tablename__: Final = "dummy_table"
-        __tableargs__: Final = {"schema": "dummy"}
+        __table_args__: Final = {"schema": "dummy"}
 
         a: Final = IntField()
         b: Final = CharField(10)
@@ -39,6 +40,7 @@ class DBDuckDBTests(DuckDBBaseTest):
         self._session = make_db_session(self.engine)
         self.dummy_df = pd.DataFrame(
             {
+                "_id": [1, 2, 3],
                 "a": [1, 4, 6],
                 "b": ["test1", "test2", "dhsjak"],
                 "json_field": [{"a": 1, "b": 2}, {"c": 3}, {"d": 4}],
@@ -49,11 +51,26 @@ class DBDuckDBTests(DuckDBBaseTest):
     def tearDown(self):
         self._drop_tables_and_schema(models=[self.DummyTable], schema="dummy")
 
+    def _find_precision(self, number: float = None) -> int:
+        if isinstance(number, float):
+            return len(str(number).split('.')[1])
+        else:
+            return 0
+
+    def _round(self, x: float = None, decimals: int = 0) -> float:
+        try:
+            return round(x, decimals)
+        except TypeError:
+            return x
+
     def _assert_col(self, session: Session, col_name: str) -> None:
         entries = session.query(self.DummyTable).all()
         col_values = [getattr(e, col_name) for e in entries]
-        expected = [
-            None if pd.isna(v) else v for v in self.dummy_df[col_name].values
+        expected = [None if pd.isna(v) else v for v in self.dummy_df[col_name].values]
+        # Slight hack to Handle imprecision in floating point numbers
+        decimals = (self._find_precision(n) for n in expected)
+        col_values = [
+            self._round(v, n_dec) if not pd.isna(v) else None for v, n_dec in zip(col_values, decimals)
         ]
         self.assertEqual(col_values, expected)
 
