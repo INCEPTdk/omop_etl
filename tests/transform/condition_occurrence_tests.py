328cc7cfcf33db2dae86cd0012b1d1cd0c9942da
Benjamin Skov Kaas-Hansen <epiben@hey.com>
2024-05-17 13:36:06 +0200

    adapt Condition Occurrence test

--- tests/transform/condition_occurrence_tests.py
+++ tests/transform/condition_occurrence_tests.py
@@ -13,18 +13,17 @@ from etl.transform.condition_occurrence import (
 )
 from etl.util.db import make_db_session, session_context
 from tests.testutils import (
-    PostgresBaseTest,
+    DuckDBBaseTest,
     base_path,
     enforce_dtypes,
     write_to_db,
 )
 
 
-class ConditionOccurenceTest(PostgresBaseTest):
+class ConditionOccurenceTest(DuckDBBaseTest):
 
     TARGET_MODEL = [OmopStem, OmopConditionOccurrence]
     LOOKUPS = [ConceptLookup, ConceptLookupStem]
-
 
     INPUT_OMOP_STEM = f"{base_path()}/test_data/condition_occurrence/in_omop_stem.csv"
     OUTPUT_FILE = f"{base_path()}/test_data/condition_occurrence/out_omop_condition_occurrence.csv"
@@ -33,11 +32,10 @@ class ConditionOccurenceTest(PostgresBaseTest):
         super().setUp()
         self._create_tables_and_schema(self.TARGET_MODEL, schema='omopcdm')
 
-
         self.omop_stem = pd.read_csv(self.INPUT_OMOP_STEM, index_col=False, sep=';')
 
         self.expected_df = pd.read_csv(self.OUTPUT_FILE, index_col=False, sep=';', parse_dates = ['condition_start_date','condition_start_datetime','condition_end_date','condition_end_datetime'])
-        self.expected_cols = [getattr(self.TARGET_MODEL[1], col) for col in self.expected_df.columns.to_list()]
+        self.expected_cols = [getattr(self.TARGET_MODEL[1], col) for col in self.expected_df.columns.to_list() if col not in {"_id"}]
 
     def tearDown(self) -> None:
         super().tearDown()
@@ -52,9 +50,12 @@ class ConditionOccurenceTest(PostgresBaseTest):
         with session_context(make_db_session(self.engine)) as session:
             condition_occurence_transformation(session)
 
-        result = select(self.expected_cols)
-        result_df = pd.read_sql(result, self.engine)
-        result_df = enforce_dtypes(self.expected_df, result_df)
+            result = select(self.expected_cols).subquery()
+            result_df = enforce_dtypes(
+                self.expected_df,
+                pd.DataFrame(session.query(result).all())
+            )
+
         pd.testing.assert_frame_equal(result_df,self.expected_df)
 
 __all__ = ['ConditionOccurenceTest']
