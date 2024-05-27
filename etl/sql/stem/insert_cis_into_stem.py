8778586394ee84928325a209137e622462c211ca
Benjamin Skov Kaas-Hansen <epiben@hey.com>
2024-05-27 16:13:43 +0200

    make left join instead of inner join to keep unmapped events

--- etl/sql/stem/insert_cis_into_stem.py
+++ etl/sql/stem/insert_cis_into_stem.py
@@ -105,7 +105,7 @@ def create_simple_stem_insert(
                 ),
             ),
         )
-        .join(
+        .outerjoin(
             Concept,
             Concept.concept_id == ConceptLookupStem.mapped_standard_code,
         )
