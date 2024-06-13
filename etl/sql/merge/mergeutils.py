25eaa80548f3f26a0cd278b91789bffd6c9a762e
daplaci <davide.placido@cpr.ku.dk>
2024-06-13 19:39:51 +0200

    add logic to remap the care site and person id

--- etl/sql/merge/mergeutils.py
+++ etl/sql/merge/mergeutils.py
@@ -98,3 +98,26 @@ def drop_duplicated_rows(
     FROM cte
     WHERE rn > 1);"""
 
+
+@clean_sql
+def remap_person_id(
+    schema: str, cdm_table: OmopCdmModelBase, person_table: OmopCdmModelBase
+):
+    """Remap Person IDs in a CDM table."""
+    return f"""SELECT  site_cdm_table.person_id AS site_person_id,
+                        merge_person.person_id AS merge_person_id
+            FROM {schema}.{cdm_table.__tablename__} AS site_cdm_table
+            INNER JOIN {schema}.{person_table.__tablename__} AS site_person
+            ON site_cdm_table.person_id = site_person.person_id
+            INNER JOIN {person_table.__table__} AS merge_person
+            ON site_person.person_source_value = merge_person.person_source_value"""
+
+
+@clean_sql
+def remap_care_site_id(schema: str, care_site_table: OmopCdmModelBase):
+    """Remap Care Site IDs in a CDM table."""
+    return f"""SELECT merge_care_site.care_site_id as merge_care_site_id,
+                    site_care_site.care_site_id as site_care_site_id
+                FROM {schema}.{care_site_table.__tablename__} AS site_care_site
+                INNER JOIN {care_site_table.__table__} AS merge_care_site
+                ON site_care_site.care_site_source_value = merge_care_site.care_site_source_value"""
