91c805d7b9e995ce8c055b8e9b58476d0712cf58
Benjamin Skov Kaas-Hansen <epiben@hey.com>
2024-07-07 19:14:12 +0200

    automatically map ATCs to ingredient with NULL quantity, which also takes care of unmapped drugs

--- etl/sql/stem/insert_drugs_into_stem.py
+++ etl/sql/stem/insert_drugs_into_stem.py
@@ -1,7 +1,7 @@
 """ SQL query string definition for the drug-related stem functions"""
 
 import os
-from typing import Any
+from typing import Any, Final
 
 from sqlalchemy import (
     DATE,
@@ -17,11 +17,16 @@ from sqlalchemy import (
     select,
     union_all,
 )
+from sqlalchemy.orm import aliased
 from sqlalchemy.sql import Insert, text
 from sqlalchemy.sql.expression import null
 from sqlalchemy.sql.functions import concat
 
 from ...models.omopcdm54.clinical import Stem as OmopStem, VisitOccurrence
+from ...models.omopcdm54.vocabulary import (
+    Concept as OmopConcept,
+    ConceptRelationship as OmopConceptRelationship,
+)
 from ...models.source import Administrations, Prescriptions
 from ...models.tempmodels import ConceptLookup, ConceptLookupStem
 from .conversions import get_conversion_factor
@@ -32,6 +37,7 @@ from .utils import (
     toggle_stem_transform,
 )
 
+CONCEPT_ID_EHR: Final[int] = 32817
 INCLUDE_UNMAPPED_CODES = os.getenv("INCLUDE_UNMAPPED_CODES", "TRUE") == "TRUE"
 
 
@@ -49,6 +55,10 @@ def get_drug_stem_insert(session: Any = None, logger: Any = None) -> Insert:
     drug_mappings = [row.__dict__ for row in drug_mappings]
 
     drugs_with_data = set(session.scalars(select(Administrations.drug_name)))
+    drugs_with_mappings = set(d["source_variable"] for d in drug_mappings)
+    drugs_without_mappings = set(
+        d for d in drugs_with_data if d not in drugs_with_mappings
+    )
 
     drug_mappings_with_data = [
         dm for dm in drug_mappings if dm["source_variable"] in drugs_with_data
@@ -103,7 +113,8 @@ def get_drug_stem_insert(session: Any = None, logger: Any = None) -> Insert:
         unique_route_source_value, Prescriptions, TEXT
     )
 
-    MappedSelectSql = (
+    # Create SELECT statement for drugs with custom mappings
+    CustomMappedSelectSql = (
         select(
             ConceptLookupStem.std_code_domain.label("domain_id"),
             VisitOccurrence.person_id,
@@ -164,28 +175,29 @@ def get_drug_stem_insert(session: Any = None, logger: Any = None) -> Insert:
                 ConceptLookup.filter == "administration_route",
             ),
         )
+        .where(Administrations.drug_name.in_(drugs_with_mappings))
     )
 
-    CteConceptLookupStemForAntijoin = (
-        select(
-            ConceptLookupStem.source_variable,
-            ConceptLookupStem.std_code_domain,
-            ConceptLookupStem.drug_exposure_type,
-        )
-        .distinct()
-        .cte("cte_concept_lookup_stem_for_antijoin")
-    )
+    # Create SELECT statement for drugs without custom mappings
+    # (they use go straight to ingredient level)
 
-    UnmappedSelectSql = (
+    OmopConcept1 = aliased(OmopConcept)
+    OmopConcept2 = aliased(OmopConcept)
+
+    AutoMappedSelectSql = (
         select(
             literal("Drug").label("domain_id"),
             VisitOccurrence.person_id,
-            null().label("concept_id"),
-            null().label("start_date"),
-            null().label("start_datetime"),
-            null().label("end_date"),
-            null().label("end_datetime"),
-            null().label("type_concept_id"),
+            OmopConcept2.concept_id.label("concept_id"),
+            cast(start_datetime, DATE).label("start_date"),
+            cast(start_datetime, TIMESTAMP).label("start_datetime"),
+            get_case_statement(
+                unique_end_datetime, Administrations, DATE
+            ).label("end_date"),
+            get_case_statement(
+                unique_end_datetime, Administrations, TIMESTAMP
+            ).label("end_datetime"),
+            literal(CONCEPT_ID_EHR).label("type_concept_id"),
             VisitOccurrence.visit_occurrence_id,
             concat(
                 Administrations.drug_name,
@@ -194,10 +206,16 @@ def get_drug_stem_insert(session: Any = None, logger: Any = None) -> Insert:
             ).label("source_value"),
             null().label("source_concept_id"),
             null().label("value_as_number"),
-            null().label("route_concept_id"),
-            null().label("route_source_value"),
+            ConceptLookup.concept_id.label("source_concept_id"),
+            route_source_value,
             null().label("era_lookback_interval"),
-            literal("unmapped_administrations").label("datasource"),
+            case(
+                (
+                    OmopConcept2.concept_id.isnot(null()),
+                    "automapped_administrations",
+                ),
+                else_="unmapped_administrations",
+            ).label("datasource"),
         )
         .select_from(Administrations)
         .join(
@@ -205,22 +223,47 @@ def get_drug_stem_insert(session: Any = None, logger: Any = None) -> Insert:
             VisitOccurrence.visit_source_value
             == concat("courseid|", Administrations.courseid),
         )
-        .outerjoin(
-            CteConceptLookupStemForAntijoin,
+        .join(
+            Prescriptions,
             and_(
-                CteConceptLookupStemForAntijoin.c.source_variable
-                == Administrations.drug_name,
-                CteConceptLookupStemForAntijoin.c.drug_exposure_type
-                == Administrations.administration_type,
+                Prescriptions.epaspresbaseid == Administrations.epaspresbaseid,
+                Prescriptions.epaspresbaseid == Prescriptions.epaspresid,
+            ),
+            isouter=INCLUDE_UNMAPPED_CODES,
+        )
+        .outerjoin(
+            ConceptLookup,
+            and_(
+                ConceptLookup.concept_string == route_source_value,
+                ConceptLookup.filter == "administration_route",
             ),
         )
-        .where(CteConceptLookupStemForAntijoin.c.std_code_domain.is_(None))
+        .join(
+            OmopConcept1,
+            OmopConcept1.concept_code == Prescriptions.epaspresdrugatc,
+            isouter=INCLUDE_UNMAPPED_CODES,
+        )
+        .join(
+            OmopConceptRelationship,
+            and_(
+                OmopConceptRelationship.concept_id_1 == OmopConcept1.concept_id,
+                OmopConceptRelationship.relationship_id
+                == "ATC - RxNorm pr lat",
+            ),
+            isouter=INCLUDE_UNMAPPED_CODES,
+        )
+        .join(
+            OmopConcept2,
+            and_(
+                OmopConcept2.concept_id == OmopConceptRelationship.concept_id_2,
+                OmopConcept2.concept_class_id == "Ingredient",
+            ),
+            isouter=INCLUDE_UNMAPPED_CODES,
+        )
+        .where(
+            Administrations.drug_name.in_(drugs_without_mappings),
+        )
     )
-
-    if INCLUDE_UNMAPPED_CODES:
-        StemSelect = union_all(MappedSelectSql, UnmappedSelectSql)
-    else:
-        StemSelect = MappedSelectSql
 
     return insert(OmopStem).from_select(
         names=[
@@ -241,6 +284,6 @@ def get_drug_stem_insert(session: Any = None, logger: Any = None) -> Insert:
             OmopStem.era_lookback_interval,
             OmopStem.datasource,
         ],
-        select=StemSelect,
+        select=union_all(CustomMappedSelectSql, AutoMappedSelectSql),
         include_defaults=False,
     )
