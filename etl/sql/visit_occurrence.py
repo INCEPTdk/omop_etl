05a301befc22e96622e143a2737fd5030e160a7f
daplaci <davide.placido@cpr.ku.dk>
2024-07-12 11:19:40 +0200

    hash visit_occurrence_id by concat shak and courseid

--- etl/sql/visit_occurrence.py
+++ etl/sql/visit_occurrence.py
@@ -145,7 +145,9 @@ def get_visit_occurrence_select(shak_code: str) -> Select:
             concat("transfromid|", CourseIdMapped.c.transfromid),
             cl2.concept_id,
             concat("chkouttoid|", CourseIdMapped.c.chkouttoid),
-            func.floor(func.hash(CourseIdMapped.c.courseid) / 2),
+            func.floor(
+                func.hash(concat(shak_code, "|", CourseIdMapped.c.courseid)) / 2
+            ),
         )
         .select_from(CourseIdMapped)
         .join(
