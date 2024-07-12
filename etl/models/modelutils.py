9911094164d31181a228a521dd1e6cc5d55c8b77
Benjamin Skov Kaas-Hansen <epiben@hey.com>
2024-07-12 16:37:16 +0200

    make DateTimeField timezone aware

--- etl/models/modelutils.py
+++ etl/models/modelutils.py
@@ -68,7 +68,7 @@ DateField: Final[Callable[[Any], Column]] = lambda *args, **kwargs: Column(
     Date, *args, **kwargs
 )
 DateTimeField: Final[Callable[[Any], Column]] = lambda *args, **kwargs: Column(
-    DateTime, *args, **kwargs
+    DateTime(timezone=True), *args, **kwargs
 )
 EnumField: Final[Callable[[Any], Column]] = lambda x, *args, **kwargs: Column(
     Enum(x), *args, **kwargs
